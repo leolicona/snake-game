2aa073f74c4eaacc7abd3578c64c2af3a47d829e
leolicona <leoncreactivo@gmail.com>
2022-01-25 08:38:13 -0600

    startGame fuction added

--- src/index.js
+++ src/index.js
@@ -17,7 +17,33 @@ let score = 0;
 let speed = 0.9;
 let intervalTime = 0;
 let interval = 0; 
+// To start and restart the game
 
+//function that deals with ALL the ove outcomes of the Snake
+    function moveOutComes() {
+        //deals with the snake hitting border or hitting self
+        if(
+            (currentSnake[0] + width >= (width * width));
+        )
+
+        
+    }
+
+function startGame() {
+    currentSnake.forEach( index => squares[index].classList.remove('snake'));
+    squares[appleIndex] = classList.remove('apple');
+        console.log( `%c squares[appleIndex] {${[appleIndex]}}`,cssConsole, squares[appleIndex]);
+    clearInterval(interval)
+    score = 0;
+    randomApple();
+    direction = 1;
+    scoreDisplay.innerText = score;
+    intervalTime = 1000;
+    currentSnake = [2, 1, 0];
+    currentIndex = 0;
+    currentSnake.forEach(index => squares[index].classList.add('snake'))
+    interval = setInterval(movesOutcomes, intervalTime);    
+}
 // Assing function to keycodes 
 
 function control(e) {
@@ -33,3 +59,6 @@ function control(e) {
         direction = + width;
     }
 }
+
+document.addEventListener('keyup', control) 
+startBtn.addEventListener('click', startGame)
